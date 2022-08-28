2a07fc4f827665d60acefc855896aceb6a01a2c2
bimashazaman <bimashazaman1234@gmail.com>
2022-08-29 00:02:42 +0600

    Creating Server

--- index.js
+++ index.js
@@ -12,12 +12,17 @@ const http = require("http");
 const app = {};
 
 // configuration
-app.config = {};
+app.config = {
+    port: 3000
+};
 
 //create server
 
 app.createServer = () => {
-  const server = http.createServer();
+  const server = http.createServer(app.handleReqRes);
+  server.listen(app.config.port, () => {
+    console.log('Server is listening on 3000');
+  })
 };
 
 //handle Request Response
@@ -25,3 +30,6 @@ app.handleReqRes = (req, res) => {
   //Response Handle
   res.end('Hello World');
 };
+
+//start the server
+app.createServer();
