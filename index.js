f3a4f58a8869c647b6b5deab33a223b980d84dd4
bimashazaman <bimashazaman1234@gmail.com>
2022-08-29 00:45:12 +0600

    Creating Server

--- index.js
+++ index.js
@@ -7,13 +7,14 @@ Date: 9/28/2022
 
 // Dependencies
 const http = require("http");
+const url = require("url");
 
 // App object- Module scaffolding
 const app = {};
 
 // configuration
 app.config = {
-    port: 3000
+  port: 3000,
 };
 
 //create server
@@ -21,15 +22,39 @@ app.config = {
 app.createServer = () => {
   const server = http.createServer(app.handleReqRes);
   server.listen(app.config.port, () => {
-    console.log('Server is listening on 3000');
-  })
+    console.log("Server is listening on 3000");
+  });
 };
 
 //handle Request Response
 app.handleReqRes = (req, res) => {
+  //Req handle
+
+  //Get the url and parse it
+  const parseUrl = url.parse(req.url, true);
+  // console.log(parseUrl)
+
+  const path = parseUrl.pathname;
+  // console.log(path);
+
+  const trimmedPath = path.replace(/^\+|\/+$/g, "");
+  // console.log(trimmedPath);
+
+  const method = req.method.toLowerCase();
+  const queryStringObject = parseUrl.query;
+
+  console.log(queryStringObject);
+
   //Response Handle
-  res.end('Hello World');
+  res.end("Hello World");
 };
 
 //start the server
 app.createServer();
+
+/*
+Title: Monitoring App
+Author: Bimasha Zaman
+Email: developerbimasha@gmail.com
+Date: 9/28/2022
+*/
