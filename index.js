c69a12ff65e065a369e4298835a717ed00d0072a
bimashazaman <bimashazaman1234@gmail.com>
2022-08-29 01:12:13 +0600

    Creating Server

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ Date: 9/28/2022
 // Dependencies
 const http = require("http");
 const url = require("url");
-const {StringDecoder} = require('string_decoder');
+const { StringDecoder } = require("string_decoder");
 
 // App object- Module scaffolding
 const app = {};
@@ -33,28 +33,25 @@ app.handleReqRes = (req, res) => {
 
   //Get the url and parse it
   const parseUrl = url.parse(req.url, true);
-  // console.log(parseUrl)
-
   const path = parseUrl.pathname;
-  // console.log(path);
-
   const trimmedPath = path.replace(/^\+|\/+$/g, "");
-  // console.log(trimmedPath);
-
   const method = req.method.toLowerCase();
   const queryStringObject = parseUrl.query;
   const headersObject = req.headers;
 
-  const decoder = new StringDecoder('utf-8');
+  const decoder = new StringDecoder("utf-8");
+  let realData = "";
 
-  req.on('data' ,(buffer) => {
+  req.on("data", (buffer) => {
+    realData += decoder.write(buffer);
+  });
 
-  })
+  req.on("end", () => {
+    realData += decoder.end();
 
-
-
-  //Response Handle
-  res.end("Hello World");
+    //Response Handle
+    res.end("Hello World");
+  });
 };
 
 //start the server
